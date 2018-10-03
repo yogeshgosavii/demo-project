107a479d46d91c4a601876619656b45d3c170f46
sumitavere <sumitavere800@gmail.com>
2018-10-03 12:07:16 +0530

    add sum.c and product.c file

---
+++ product.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,product;
+printf("Enter two numbers :");
+scanf("%d %d",&a,&b);
+product=a+b;
+printf("Product is : %d\n",product);
+}
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+ int a,b,sum;
+ printf("ENTER THE TWO NUMBER: ");
+ scanf("%d %d",&a,&b);
+ sum=a+b;
+ printf("\n Sum of Two NUmber is : %d\n",sum);
+ 
+}
